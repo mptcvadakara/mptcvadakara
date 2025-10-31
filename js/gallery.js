32edf288765578e44af7c09b0cce0bb72612d048
mptcvadakara <mptcvdk@gmail.com>
2025-10-31 22:31:36 +0530

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -3,12 +3,10 @@
 // The paths MUST be relative to the HTML file (gallery.html is in 'source/').
 // Use '../gallery/' to correctly reference images from the HTML file's location.
 const imageFiles = [
-    '../gallery/image1.jpg',
-    '../gallery/image2.png',
-    '../gallery/image3.jpg',
-    '../gallery/image4.jpeg', // Example: Add more images as needed
-    '../gallery/mptc.jpg',
-    // ... add all your image paths here ...
+    '../gallery/1.jpg', '../gallery/2.jpg', '../gallery/3.jpg', '../gallery/4.jpg', '../gallery/5.jpg', 
+    '../gallery/6.jpg', '../gallery/7.jpg', '../gallery/8.jpg', '../gallery/9.jpg', '../gallery/10.jpg',
+    '../gallery/11.jpg', '../gallery/12.jpg', '../gallery/13.jpg', '../gallery/14.jpg', '../gallery/15.jpg', 
+    '../gallery/mptc.jpg' 
 ];
 
 // --- 2. Gallery and Lightbox DOM Elements ---
@@ -107,3 +105,4 @@ lightbox.addEventListener('click', function(event) {
         closeLightbox();
     }
 });
+
