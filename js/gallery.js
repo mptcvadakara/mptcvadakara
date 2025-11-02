50acbb699f42db371d487a3542635942d21191ba
mptcvadakara <mptcvdk@gmail.com>
2025-11-03 00:47:08 +0530

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -3,10 +3,11 @@
 // The paths MUST be relative to the HTML file (gallery.html is in 'source/').
 // Use '../gallery/' to correctly reference images from the HTML file's location.
 const imageFiles = [
-    '../gallery/1.jpg', '../gallery/2.jpg', '../gallery/3.jpg', '../gallery/4.jpg', '../gallery/5.jpg', 
-    '../gallery/6.jpg', '../gallery/7.jpg', '../gallery/8.jpg', '../gallery/9.jpg', '../gallery/10.jpg',
-    '../gallery/11.jpg', '../gallery/12.jpg', '../gallery/13.jpg', '../gallery/14.jpg', '../gallery/15.jpg', 
-    '../gallery/mptc.jpg' 
+    '../gallery/mptc.jpg', '../gallery/1.jpg', '../gallery/2.jpg', '../gallery/3.jpg', '../gallery/4.jpg', '../gallery/5.jpg', 
+    '../gallery/6.jpg', '../gallery/7.jpg', '../gallery/8.jpg', '../gallery/9.jpg', '../gallery/10.jpg','../gallery/11.jpg', 
+    '../gallery/12.jpg', '../gallery/13.jpg', '../gallery/14.jpg', '../gallery/15.jpg', '../gallery/17.jpg',  '../gallery/17.jpg',  
+    '../gallery/18.jpg',  '../gallery/19.jpg',  '../gallery/20.jpg',  '../gallery/21.jpg',  '../gallery/22.jpg',  '../gallery/23.jpg',  
+    '../gallery/24.jpg',  '../gallery/25.jpg',  '../gallery/26.jpg',  '../gallery/27.jpg',  '../gallery/28.jpg',  
 ];
 
 // --- 2. Gallery and Lightbox DOM Elements ---
@@ -105,4 +106,5 @@ lightbox.addEventListener('click', function(event) {
         closeLightbox();
     }
 });
+
 
