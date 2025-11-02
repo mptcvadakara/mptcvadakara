4bb39efd5d704a6df4303ab4e9224233fdcb12d3
mptcvadakara <mptcvdk@gmail.com>
2025-11-03 00:47:45 +0530

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -7,7 +7,7 @@ const imageFiles = [
     '../gallery/6.jpg', '../gallery/7.jpg', '../gallery/8.jpg', '../gallery/9.jpg', '../gallery/10.jpg','../gallery/11.jpg', 
     '../gallery/12.jpg', '../gallery/13.jpg', '../gallery/14.jpg', '../gallery/15.jpg', '../gallery/17.jpg',  '../gallery/17.jpg',  
     '../gallery/18.jpg',  '../gallery/19.jpg',  '../gallery/20.jpg',  '../gallery/21.jpg',  '../gallery/22.jpg',  '../gallery/23.jpg',  
-    '../gallery/24.jpg',  '../gallery/25.jpg',  '../gallery/26.jpg',  '../gallery/27.jpg',  '../gallery/28.jpg',  
+    '../gallery/24.jpg',  '../gallery/25.jpg',  '../gallery/26.jpg',  '../gallery/27.jpg',  '../gallery/28.jpg'  
 ];
 
 // --- 2. Gallery and Lightbox DOM Elements ---
@@ -106,5 +106,6 @@ lightbox.addEventListener('click', function(event) {
         closeLightbox();
     }
 });
+
 
 
